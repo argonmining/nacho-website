707af5e222dbbddec886a998f6b2a9b608db538f
argonmining <ashton@onargon.com>
2024-12-29 19:02:46 -0800

    hero rotate

--- src/components/ui/FloatingNacho.tsx
+++ src/components/ui/FloatingNacho.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 export default function FloatingNacho() {
   return (
     <motion.div
-      className="absolute left-0 -top-[220px] w-[250px] transform md:-top-[300px] md:w-[350px]"
+      className="absolute left-0 -top-[180px] w-[250px] -rotate-45 transform md:-top-[250px] md:w-[350px]"
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, delay: 0.5 }}
-      whileHover={{ rotate: -2, transition: { duration: 0.2 } }}
+      whileHover={{ rotate: -42, transition: { duration: 0.2 } }}
     >
       <Image
         src="/img/nacho-cool.webp"
