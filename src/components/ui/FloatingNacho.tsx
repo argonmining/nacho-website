765bc07a4c4cef08d698413b889c109148de4e91
argonmining <ashton@onargon.com>
2024-12-29 20:55:45 -0800

    space

--- src/components/ui/FloatingNacho.tsx
+++ src/components/ui/FloatingNacho.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function FloatingNacho() {
   return (
     <motion.div
-      className="absolute left-0 -top-[190px] w-[250px] md:-top-[355px] md:w-[455px]"
+      className="absolute left-0 -top-[195px] w-[250px] md:-top-[355px] md:w-[455px]"
       initial={{ opacity: 0, y: -50, rotate: 0 }}
       animate={{ opacity: 1, y: 0, rotate: -2 }}
       transition={{ duration: 0.8, delay: 0.5 }}
